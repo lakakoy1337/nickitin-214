64c49bd1550b2215f32eba6a214bb7456c2d6f6b
lakakoy1337 <zaqw1151@gmail.com>
2022-09-22 09:31:59 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("                   .............                     @@@@@        @ @   @     @                     .............")
+print("                    ............                    @     @     @   @   @     @                     ............")
+print("                     ...........                      @@@     @     @   @@@@@@@                     ...........")
+print("                      ..........                     @@             @         @                     ..........")
+print("                       .........                     @@@@@          @         @                     .........")
+print("                        ........                 __________________________________                 ........")
+print("                         .......                                                                    .......")
+print("                          ......                            H E L L O                               ......")
+print("                           .....                 __________________________________                 .....")
+print("                            ....                    @@@@   @@@@   @@@  @    @ @@@@                  ....")
+print("                             ...                   @       @   @ @   @ @    @ @  @                  ...")
+print("                             | |                   @  @@@  @@@   @   @ @    @ @@@@                  | |")
+print("                             | |                   @   @   @  @  @   @ @    @ @                     | |     ")
+print("                  ___________| |__________          @@@@   @   @  @@@   @@@@  @          ___________| |__________")
